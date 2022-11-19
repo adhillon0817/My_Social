b70d573a77a6d793afe9a684bcf8bd3ddcd89f13
adhillon0817 <avleendhillon@gmail.com>
2022-11-18 22:32:09 -0800

    aded friends to schema

--- models/User.js
+++ models/User.js
@@ -24,7 +24,14 @@ const UserSchema = new Schema({
         },
     ],
 
-    
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        }
+    ]
+
+
 
 
 })
