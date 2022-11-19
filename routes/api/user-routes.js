a216d138fb57e1abe284cf30eb9f97a9142372e2
adhillon0817 <avleendhillon@gmail.com>
2022-11-18 17:37:13 -0800

    set up user-routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,7 +5,35 @@ const {
     createUser,
     deleteUser,
     updateUser,
-    addFriend,
-    removeFriend
+    addFriends,
+    removeFriends
 } = require('../../controllers/user-controller')
 
+// /api/users creation
+router
+.route('/')
+.get(getUsers)
+.post(createUser)
+
+
+// get user by id and change user
+router
+.route('/:id')
+.get(getUsersById)
+.delete(deleteUser)
+.put(updateUser)
+
+
+//adding and deleting user friends
+router
+.route("/:id/friends/:friendId")
+.post(addFriends)
+.delete(removeFriends);
+
+
+
+
+
+
+
+module.exports = router;
