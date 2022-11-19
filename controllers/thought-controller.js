07cd3aa2e0a87b32375a799307e2b2469d7f43b3
adhillon0817 <avleendhillon@gmail.com>
2022-11-18 20:09:04 -0800

    created thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -11,18 +11,34 @@ module.exports = {
 
 // GET THOUGHT BY ID
     getThoughtbyId(req, res) {
-        Thoughts.findOne({_id: req.params.id})
+        Thoughts.findOne({_id: req.params.thoughtid})
         .select('-__v')
       .then((thoughts) =>
         !thoughts
           ? res.status(404).json({ message: 'No thoughts with that ID' })
           : res.json(thoughts)
       )
-
       .catch((err) => res.status(500).json(err));
     },
 
 
-    
+//CREATE THOUGHT
+
+createThought(req, res) {
+    Thoughts.create(req.body) 
+        .then((thoughts) => {
+            return Users.findOneAndUpdate(
+                {_id:req.body.id}, {$addtoSet: {thoughts: thoughtId}}, {new: true}
+            );
+        })
+        .then((users) =>
+        !users
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json('thoughts!')
+        )
+        .catch((err) => {res.status(500).json(err)});
+    }
+
+
 
 }
